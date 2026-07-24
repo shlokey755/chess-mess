327fd59880d96943116453aaf15c630daac0422c
s <shlokgupta.cr7@gmail.com>
2026-07-24 16:30:58 +0530

    Create GameState object with reset and logging
    Initialize GameState object with properties and methods.

---
+++ js/state.js
@@ -0,0 +1,33 @@
+const GameState = {
+  board: [],
+  units: {},
+  turn: 0,
+  currentPlayer: 1,
+  selectedUnit: null,
+  validMoves: [],
+  boardSize: 6,
+  log: [],
+
+  reset() {
+    this.board = Array(6).fill(null).map(() => Array(6).fill('grass'));
+    this.units = {
+      'p1-knight': { type: 'knight', player: 1, pos: [0, 2], hp: 3 },
+      'p1-mage': { type: 'mage', player: 1, pos: [0, 3], hp: 2 },
+      'p1-brawler': { type: 'brawler', player: 1, pos: [0, 4], hp: 4 },
+      'p2-knight': { type: 'knight', player: 2, pos: [5, 2], hp: 3 },
+      'p2-mage': { type: 'mage', player: 2, pos: [5, 3], hp: 2 },
+      'p2-brawler': { type: 'brawler', player: 2, pos: [5, 4], hp: 4 }
+    };
+    this.turn = 0;
+    this.currentPlayer = 1;
+    this.selectedUnit = null;
+    this.validMoves = [];
+    this.boardSize = 6;
+    this.log = ['Game started!'];
+  },
+
+  addLog(msg) {
+    this.log.push(msg);
+    if (this.log.length > 5) this.log.shift();
+  }
+};
